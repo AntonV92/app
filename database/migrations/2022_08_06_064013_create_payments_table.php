0dfb53cbd534d742ccaac815b9b8edc32c3120d9
AntonV92 <av476347@gmail.com>
2022-08-06 10:27:04 +0300

    add migration

---
+++ database/migrations/2022_08_06_064013_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->bigInteger('merchant_id');
+            $table->bigInteger('merchant_pid');
+            $table->decimal('amount');
+            $table->bigInteger('amount_cents');
+            $table->string('status', 20);
+            $table->string('currency', 3);
+            $table->string('gateway_name', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
